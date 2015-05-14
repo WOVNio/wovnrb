dadda48aa319e08549d6a67407dd294d17aa0010
Jeff Sandford <jeff@minimaltech.co>
2015-05-14 19:01:28 +0900

    alpha4

--- lib/wovnrb.rb
+++ lib/wovnrb.rb
@@ -34,7 +34,6 @@ module Wovnrb
               }
         switch_lang(body, values, url, lang) unless status.to_s =~ /^1|302/ || lang === DEFAULT_LANG
         #d = Dom.new(storage.get_values, body, lang)
-        #body[0] = transform_body(body, lang)
       end
 
       headers.out(res_headers)
@@ -42,73 +41,14 @@ module Wovnrb
       #[status, res_headers, d.transform()]
     end
 
-    #def transform_request(env, def_lang=DEFAULT_LANG)
-    #  # get subdomain -> match group 1
-    #  match = env["SERVER_NAME"].match(/^([^.]+)\.[^.]+\./)
-    #  changed_vals = {"HTTP_HOST" => env["HTTP_HOST"],
-    #                  "HTTP_REFERER" => env["HTTP_REFERER"],
-    #                  "SERVER_NAME" => env["SERVER_NAME"]}
-    #  if match && Lang::LANG[match[1]]
-    #    changed_vals[:lang] = match[1]
-    #    #env["HTTP_HOST"] = env["HTTP_HOST"].sub(/^([^.]*\.)?([^.]+\..+)$/, '\2')
-    #    env["HTTP_HOST"] = env["HTTP_HOST"].sub("#{match[1]}.", '')
-    #    env["HTTP_REFERER"] = env["HTTP_REFERER"].sub("#{match[1]}.", '') if env["HTTP_REFERER"]
-    #    env["SERVER_NAME"] = env["SERVER_NAME"].sub("#{match[1]}.", '')
-    #  end
-    #  changed_vals[:lang] || def_lang
-    #end
-
-    def transform_body(body, lang=DEFAULT_LANG)
-
-      # key = body[0].match(/data-wovnio="key=([^"]+)/)[1]
-      # get the url of the requested page
-      # escape_url = env["rack.url_scheme"] + env["HTTP_HOST"] + env["PATH_INFO"]
-      # get the values from the requested page
-      # TODO store these values and simply check file
-      response = Net::HTTP.get_response URI.parse("https://j.wovn.io/cdn/0/2Wle3/?u=https%3A%2F%2Fwovn.io%2Fdashboard")
-      location = response['location']
-      cdn = Net::HTTP.get URI.parse(location)
-      res_object_string = cdn.match(/\{\"id[\s\S]*\}\}\}/)
-      res_object = JSON.parse(res_object_string.to_s)
-      element_values_raw = res_object["element_values"]
-      original_lang = res_object["language"]
-      element_values = {}
-      # fill in the values hash
-      element_values_raw.each do |ele_val|
-        element_values[ele_val["src_body"]] = {} if element_values[ele_val["src_body"]].nil?
-        element_values[ele_val["src_body"]][ele_val["language"]] = [] if element_values[ele_val["src_body"]][ele_val["language"]].nil?
-        element_values[ele_val["src_body"]][original_lang] = [] if element_values[ele_val["src_body"]][original_lang].nil?
-        count = element_values[ele_val["src_body"]][ele_val["language"]].length
-        element_values[ele_val["src_body"]][ele_val["language"]][count] = {"xpath" => ele_val["xpath"], "data" => ele_val["body"]}
-        element_values[ele_val["src_body"]][original_lang][count] = {"xpath" => ele_val["xpath"], "data" => ele_val["src_body"]}        
-      end
-
-      lang = 'ja'
-      d = Dom::Dom.new(element_values, body[0], lang)
-      res = d.transform()
-      res
-
-    end
 
     def switch_lang(body, values, url, lang=DEFAULT_LANG)
+      return if values.size == 0
       def_lang = 'en'
       text_index = values['text_vals']
       src_index = values['img_vals'] || {}
       img_src_prefix = values['img_src_prefix'] || ''
       string_index = {}
-      #values.each do |v|
-      #  src_data = v['src_body'].strip
-      #  data = v['body'].strip != "" ? v['body'].strip : "\u200b"
-      #  lang = v['language']
-      #  if !string_index.has_key?(src_data)
-      #    string_index[src_data] = {}
-      #    string_index[src_data][def_lang] = [];
-      #  end
-      #  # iterator doesn't support [1]
-      #  string_index[src_data][def_lang].push({'xpath' => v['xpath'].gsub(/\[1\]/, ''), 'data' => src_data})
-      #  string_index[src_data][lang] = [] if !string_index[src_data].has_key?(lang)
-      #  string_index[src_data][lang].push({'xpath' => v['xpath'].gsub(/\[1\]/, ''), 'data' => data})
-      #end
       body.map! do |b|
         d = Nokogiri::HTML5(b)
         d.xpath('//text()').each do |node|
@@ -137,6 +77,12 @@ module Wovnrb
             end
           end
         end
+        # INSERTS
+        insert_node = Nokogiri::XML::Node.new('script', d)
+        insert_node['type'] = 'text/javascript'
+        insert_node.content = "window.wovn_backend = function() { return {'currentLang': '#{lang}'}; };"
+        parent_node = d.at_css('head') || d.at_css('body') || d.at_css('html')
+        parent_node.add_child(insert_node)
         d.to_html
       end
     end
--- lib/wovnrb/store.rb
+++ lib/wovnrb/store.rb
@@ -14,6 +14,8 @@ module Wovnrb
           'url_pattern_name' => 'subdomain',
           'url_pattern_reg' => "^(?<lang>[^.]+)\.",
           'query' => [],
+          'backend_host' => 'rs1.wovn.io',
+          'backend_port' => '6379',
         }
       @config_loaded = false
     end
@@ -21,13 +23,18 @@ module Wovnrb
     def get_settings
       if !@config_loaded
         if Rails.configuration.respond_to? :wovnrb
-          @settings.merge!(Rails.configuration.wovnrb)
+          @settings.merge!(Rails.configuration.wovnrb.stringify_keys)
+          @settings['backend_port'] = @settings['backend_port'].to_s
         end
         user_token = @settings['user_token']
-        user_token = 'lYWQ9'
+        #user_token = 'lYWQ9'
         redis_key = 'WOVN:BACKEND:SETTING::' + user_token
-        cli = Redis.new(host: 'rs1.wovn.io', port: '6379')
-        vals = cli.hgetall(redis_key) || {}
+        cli = Redis.new(host: @settings['backend_host'], port: @settings['backend_port'])
+        begin
+          vals = cli.hgetall(redis_key) || {}
+        rescue
+          vals = {}
+        end
         if vals.has_key?('query')
           vals['query'] = JSON.parse(vals['query'])
         end
@@ -38,9 +45,9 @@ module Wovnrb
     end
 
     def get_values(url)
-      url = 'http://wovn.io'
+      #url = 'http://wovn.io'
       user_token = self.get_settings['user_token']
-      user_token = 'lYWQ9'
+      #user_token = 'lYWQ9'
       redis_key = 'WOVN:BACKEND:STORAGE:' + url.gsub(/\/$/, '') + ':' + user_token
       request_values(redis_key)
       #f = File.open('./values/values', 'r')
@@ -49,9 +56,13 @@ module Wovnrb
     end
 
     def request_values(key)
-      cli = Redis.new(host: 'rs1.wovn.io', port: '6379')
-      vals = cli.get(key) || '{}'
-      vals = JSON.parse(vals)
+      cli = Redis.new(host: @settings['backend_host'], port: @settings['backend_port'])
+      begin
+        vals = cli.get(key) || '{}'
+        vals = JSON.parse(vals)
+      rescue
+        vals = {}
+      end
     end
 
   end
