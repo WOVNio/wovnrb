1bb4e25238a242efddbd5536ba7a8e8d7a7311b9
Masahiro Iuchi <iuchi@wovn.io>
2016-03-15 16:10:09 +0000

    Rewrite URLs in form tag's action property. #13

--- lib/wovnrb.rb
+++ lib/wovnrb.rb
@@ -150,11 +150,33 @@ module Wovnrb
 
         # add lang code to anchors href if not default lang 
         if lang != STORE.settings['default_lang']
+          pattern = STORE.settings['url_pattern']
+
           d.xpath('//a').each do |a|
             next if check_wovn_ignore(a)
             href = a.get_attribute('href')
-            new_href = add_lang_code(href, STORE.settings['url_pattern'], lang, headers)
+            new_href = add_lang_code(href, pattern, lang, headers)
             a.set_attribute('href', new_href)
+          end
+
+          d.xpath('//form').each do |form|
+            next if check_wovn_ignore(form)
+            method = form.get_attribute('method')
+            if pattern == 'query' && (method.nil? || method.upcase == 'GET')
+              insert_node = Nokogiri::XML::Node.new('input', d)
+              insert_node['type'] = 'hidden'
+              insert_node['name'] = 'wovn'
+              insert_node['value'] = lang
+              if form.children.size > 0
+                form.children.first.add_previous_sibling(insert_node)
+              else
+                form.add_child(insert_node)
+              end
+            else
+              action = form.get_attribute('action')
+              new_action = add_lang_code(action, pattern, lang, headers)
+              form['action'] = new_action
+            end
           end
         end
 
