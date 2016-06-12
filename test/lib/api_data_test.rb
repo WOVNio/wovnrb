af41f3164f96590846fb4c506de4a638c703297a
Masahiro Iuchi <iuchi@wovn.io>
2016-06-12 14:08:58 +0000

    Fix failed tests.

--- test/lib/api_data_test.rb
+++ test/lib/api_data_test.rb
@@ -16,78 +16,81 @@ class Wovnrb
     end
 
     def test_initialize
-      Wovnrb::ApiData.new('http://wwww.example.com', Wovnrb::Store.instance)
+      Wovnrb::ApiData.new(Wovnrb::Store.instance)
     end
 
-    def test_get_data
+    def test_get_page_values
       token = 'a'
       url = 'url'
       stub_request(:get, "https://api.wovn.io/v0/values?token=#{token}&url=#{url}").
         to_return(:body => '{"test_body": "a"}')
       store = Wovnrb::Store.instance
       store.settings['user_token'] = token
-      api_data = Wovnrb::ApiData.new(url, store)
+      api_data = Wovnrb::ApiData.new(store)
 
-      assert_equal({'test_body' => 'a'}, api_data.get_data)
+      assert_equal({'test_body' => 'a'}, api_data.get_page_values(url))
     end
 
-    def test_get_data_when_cache_exists
+    def test_get_page_values_when_cache_exists
       token = 'a'
       url = 'url'
       stub = stub_request(:get, "https://api.wovn.io/v0/values?token=#{token}&url=#{url}").
         to_return(:body => '{"test_body": "a"}')
       store = Wovnrb::Store.instance
       store.settings['user_token'] = token
-      api_data = Wovnrb::ApiData.new(url, store)
+      api_data = Wovnrb::ApiData.new(store)
 
-      assert_equal({'test_body' => 'a'}, api_data.get_data)
-      assert_equal({'test_body' => 'a'}, api_data.get_data)
+      assert_equal({'test_body' => 'a'}, api_data.get_page_values(url))
+      assert_equal({'test_body' => 'a'}, api_data.get_page_values(url))
       assert_requested(stub, :times => 1)
     end
 
-    def test_get_data_fail
+    def test_get_page_values_fail
       token = 'a'
       url = 'url'
       stub_request(:get, "https://api.wovn.io/v0/values?token=#{token}&url=#{url}").
         to_return(:status => [500, "Internal Server Error"])
       store = Wovnrb::Store.instance
       store.settings['user_token'] = token
-      api_data = Wovnrb::ApiData.new(url, store)
+      api_data = Wovnrb::ApiData.new(store)
       log_mock = Wovnrb::LogMock.mock_log
 
-      assert_equal({}, api_data.get_data)
+      assert_equal({}, api_data.get_page_values(url))
       assert(log_mock.errors[0].start_with?('API server GET request failed'))
     end
 
-    def test_build_api_uri
+    def test_build_page_values_uri
       token = 'a'
       url = 'url'
       store = Wovnrb::Store.instance
       store.settings['user_token'] = token
-      api_data = Wovnrb::ApiData.new(url, store)
-      api_uri = api_data.send(:build_api_uri)
+      api_data = Wovnrb::ApiData.new(store)
+      api_data.instance_variable_set(:@access_url, url)
+      api_uri = api_data.send(:build_page_values_uri)
       assert_equal('https://api.wovn.io/v0/values?token=a&url=url', api_uri.to_s)
     end
 
-    def test_build_api_uri_with_old_api_url
+    def test_build_page_values_uri_with_old_api_url
       token = 'a'
       url = 'url'
       store = Wovnrb::Store.instance
       store.settings['user_token'] = token
       store.settings['api_url'] = 'https://api.wovn.io/v0/values'
-      api_data = Wovnrb::ApiData.new(url, store)
-      api_uri = api_data.send(:build_api_uri)
+      api_data = Wovnrb::ApiData.new(store)
+      api_data.instance_variable_set(:@access_url, url)
+      api_uri = api_data.send(:build_page_values_uri)
       assert_equal('https://api.wovn.io/v0/values?token=a&url=url', api_uri.to_s)
     end
 
-    def test_build_api_uri_with_non_default_api_url
+    def test_build_page_values_uri_with_non_default_api_url
       token = 'a'
       url = 'url'
       store = Wovnrb::Store.instance
       store.settings['user_token'] = token
       store.settings['api_url'] = 'http://api0.wovn.io/v1'
-      api_data = Wovnrb::ApiData.new(url, store)
-      api_uri = api_data.send(:build_api_uri)
+      api_data = Wovnrb::ApiData.new(store)
+      api_data.instance_variable_set(:@access_url, url)
+      api_uri = api_data.send(:build_page_values_uri)
       assert_equal('http://api0.wovn.io/v1/values?token=a&url=url', api_uri.to_s)
     end
   end
