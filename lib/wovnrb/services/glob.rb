d6b4bc7e71afd0c25e7f20e8341e6433cd0eab41
mrasu <m.rasu.hitsuji@gmail.com>
2016-10-11 11:29:16 +0900

    Add setting to not swap in the specified glob (#51)
    * Add setting to not swap in the specified glob
    
    * change config's name and fix english

---
+++ lib/wovnrb/services/glob.rb
@@ -0,0 +1,25 @@
+module Wovnrb
+  # inspired by https://github.com/isaacs/node-glob
+  #
+  # "*" Matches 0 or more characters in a single path portion
+  # "**" If a "globstar" is alone in a path portion,
+  #   then it matches zero or more directories and subdirectories searching for matches.
+  #
+  # @note "?" or other pattern is not implemented
+  class Glob
+    def initialize(pattern)
+      sub_directories = pattern.split('/**', -1)
+      regexp = sub_directories.map do |sub_dir|
+        sub_dir.split('*', -1)
+          .map {|p| Regexp.escape(p)}
+          .join('[^/]*')
+      end.join('(/[^/]*)*')
+
+      @regexp = Regexp.new("^#{regexp}$")
+    end
+
+    def match?(url)
+      !@regexp.match(url).nil?
+    end
+  end
+end
--- test/lib/store_test.rb
+++ test/lib/store_test.rb
@@ -43,5 +43,45 @@ module Wovnrb
       assert_equal(false, valid)
       assert_equal(['User token  is not valid.', 'Secret key  is not valid.'], mock.errors)
     end
+
+    def test_settings_ignore_paths
+      s = Wovnrb::Store.instance
+      s.settings({'ignore_paths' => ['/api/**']})
+      assert_equal(1, s.settings['ignore_globs'].size)
+      assert_equal(true, s.settings['ignore_globs'].first.match?('/api/a/b'))
+      assert_equal(false, s.settings['ignore_globs'].first.match?('/a/b'))
+    end
+
+    def test_settings_ignore_paths_multiple
+      s = Wovnrb::Store.instance
+      s.settings({'ignore_paths' => ['/api/a/**', '/api/b/**']})
+      assert_equal(2, s.settings['ignore_globs'].size)
+      assert_equal(true, s.settings['ignore_globs'].any?{|g| g.match?('/api/a')})
+      assert_equal(true, s.settings['ignore_globs'].any?{|g| g.match?('/api/b')})
+      assert_equal(false, s.settings['ignore_globs'].any?{|g| g.match?('/api/c')})
+    end
+
+    def test_settings_ignore_paths_empty
+      s = Wovnrb::Store.instance
+      s.settings({'ignore_paths' => []})
+      assert_equal([], s.settings['ignore_globs'])
+    end
+
+    def test_settings_invalid_ignore_paths
+      mock = LogMock.mock_log
+      store = Wovnrb::Store.instance
+      store.settings({'ignore_paths' => 'aaaa'})
+
+      assert_equal(false, store.valid_settings?)
+      assert_equal(['User token  is not valid.', 'Secret key  is not valid.', 'Ignore Paths aaaa should be Array.'], mock.errors)
+    end
+
+    def test_settings_ignore_glob_injection
+      s = Wovnrb::Store.instance
+      s.settings({'ignore_paths' => nil})
+      s.settings({'ignore_globs' => [1, 2]})
+
+      assert_equal([], s.settings['ignore_globs'])
+    end
   end
 end
