8b7e8924b96db0140fb1b4c144e3c8e33545fe2f
Adrien Bougouin <adrien@wovn.io>
2016-12-26 11:42:01 +0900

    test+ fix unballanced src-dst content

--- lib/wovnrb/html_replacers/html_text_replacer.rb
+++ lib/wovnrb/html_replacers/html_text_replacer.rb
@@ -29,34 +29,69 @@ module Wovnrb
     end
 
     private
+    # Swaps the content of a node by the content of a given string.
+    #
+    # @param [Nokogiri::XML::Node] node The node from which content must be
+    #                                   swapped.
+    # @param [String] The content to put in the node.
     def swap_val(node, dst)
       if node.name.downcase != 'text'
-        swap_complex_val(node, dst)
+        dst_node = data_to_node(dst)
+        swap_complex_val(node, dst_node)
       else
         node.content = replace_text(node.content, dst)
       end
     end
 
-    def swap_complex_val(node, dst)
-      dst_node = (dst.is_a? String) ? data_to_node(dst) : dst
-      node_children = node.children
+    # Swaps the content of a node by the content of a given node.
+    #
+    # @param [Nokogiri::XML::Node] src_node The node from which content must be
+    #                                       swapped.
+    # @param [Nokogiri::XML::Node] dst_node The node with the content to put in
+    #                                       the src_node.
+    def swap_complex_val(src_node, dst_node)
+      align_node_to_src(src_node, dst_node)
+      src_node_children = src_node.children
       dst_node_children = dst_node.children
 
-      node_children.each_with_index do |child, i|
+      src_node_children.each_with_index do |src_child, i|
         dst_child = dst_node_children[i]
 
-        if child.name.downcase == 'text'
-          child.content = replace_text(child.content.strip, dst_child.content.strip)
+        if src_child.name.downcase == 'text'
+          src_child.content = replace_text(src_child.content.strip, dst_child.content)
         else
-          swap_complex_val(child, dst_child)
+          swap_complex_val(src_child, dst_child)
         end
       end
 
     end
 
+    # Creates a node from a HTML string.
+    #
+    # @param [String] str The string to act as inner HTML.
+    #
+    # @return [Nokogiri::XML::Node] The node represented be str.
     def data_to_node(str)
       dom = Nokogiri::HTML5("<html><body><div id=\"dst-node\">#{str}</div></body></html>")
       return dom.xpath("//div[@id='dst-node']").first
+    end
+
+    # Aligns two nodes for swaping.
+    # If one node has more children, then it should be a text node a the
+    # begginning or the end. If so, an empty text node is added to the node with
+    # less children.
+    #
+    # @param [Nokogiri::XML::Node] node_1 The first node to align.
+    # @param [Nokogiri::XML::Node] node_2 The second node to align.
+    def align_node_to_src(node_1, node_2)
+      base_node = (node_1.children.count > node_2.children.count) ? node_1 : node_2
+      node_to_adjust = (base_node == node_1) ? node_2 : node_1
+
+      if base_node.children.first.name.downcase == 'text' && node_to_adjust.children.first.name.downcase != 'text'
+        node_to_adjust.inner_html = ' ' + node_to_adjust.inner_html
+      elsif base_node.children.last.name.downcase == 'text' && node_to_adjust.children.last.name.downcase != 'text'
+        node_to_adjust.inner_html += ' '
+      end
     end
   end
 end
---
+++ test/lib/html_replacers/html_text_replacer_test.rb
@@ -0,0 +1,109 @@
+# -*- encoding: UTF-8 -*-
+require 'test_helper'
+require 'webmock/minitest'
+
+module Wovnrb
+  class HTMLTextReplacerTest < WovnMiniTest
+    def test_replace
+      replacer = HTMLTextReplacer.new({
+        'Hello' => {'ja' => [{'data' => 'こんにちは'}]}
+      }, {})
+
+      dom = Wovnrb.get_dom('Hello')
+      replacer.replace(dom, Lang.new('ja'))
+
+      content = dom.xpath('//text()')[0].content
+      assert_equal('こんにちは', content)
+    end
+
+    def test_replace_multiple
+      replacer = HTMLTextReplacer.new({
+        'Hello' => {'ja' => [{'data' => 'こんにちは'}]},
+        'Bye' => {'ja' => [{'data' => 'さようなら'}]}
+      }, {})
+
+      dom = Wovnrb.get_dom('<span>Hello</span><span>Bye</span>')
+      replacer.replace(dom, Lang.new('ja'))
+
+      content = dom.xpath('//text()')[0].content
+      content2 = dom.xpath('//text()')[1].content
+      assert_equal('こんにちは', content)
+      assert_equal('さようなら', content2)
+    end
+
+    def test_replace_wovn_ignore
+      replacer = HTMLTextReplacer.new({
+        'Hello' => {'ja' => [{'data' => 'こんにちは'}]}
+      }, {})
+
+      dom = Wovnrb.get_dom('<div id="test" wovn-ignore>Hello</div>')
+      replacer.replace(dom, Lang.new('ja'))
+
+      content = dom.xpath('//text()')[0].content
+      assert_equal('Hello', content)
+    end
+
+    def test_replace_with_complex_value
+      replacer = HTMLTextReplacer.new({}, {
+        'Hello <a>World</a>' => {'ja' => [{'data' => 'こんにちは<a>World</a>'}]}
+      })
+
+      dom = Wovnrb.get_dom('<p>Hello <a>World</a></p>')
+      replacer.replace(dom, Lang.new('ja'))
+
+      inner_html = dom.xpath('//p')[0].inner_html
+      assert_equal('こんにちは<a>World</a>', inner_html)
+    end
+
+    def test_replace_multiple_with_complex_value
+      replacer = HTMLTextReplacer.new({}, {
+        'Hello <a>World</a>' => {'ja' => [{'data' => 'こんにちは<a>World</a>'}]},
+        'Another Hello <a>World ;)</a>' => {'ja' => [{'data' => 'こんにちは<a>World ;)</a>'}]},
+      })
+
+      dom = Wovnrb.get_dom('<p>Hello <a>World</a></p><p>Another Hello <a>World ;)</a></p>')
+      replacer.replace(dom, Lang.new('ja'))
+
+      inner_html_1 = dom.xpath('//p')[0].inner_html
+      inner_html_2 = dom.xpath('//p')[1].inner_html
+      assert_equal('こんにちは<a>World</a>', inner_html_1)
+      assert_equal('こんにちは<a>World ;)</a>', inner_html_2)
+    end
+
+    def test_replace_wovn_ignore_with_complex_value
+      replacer = HTMLTextReplacer.new({}, {
+        'Hello <a>World</a>' => {'ja' => [{'data' => 'こんにちは<a>World</a>'}]}
+      })
+
+      dom = Wovnrb.get_dom('<p wovn-ignore>Hello <a>World</a></p>')
+      replacer.replace(dom, Lang.new('ja'))
+
+      inner_html = dom.xpath('//p')[0].inner_html
+      assert_equal('Hello <a>World</a>', inner_html)
+    end
+
+    def test_replace_with_complex_value_unballanced_left_alignment
+      replacer = HTMLTextReplacer.new({}, {
+        '<a>Hello World</a>' => {'ja' => [{'data' => 'こんにちは<a>World</a>'}]}
+      })
+
+      dom = Wovnrb.get_dom('<p><a>Hello World</a></p>')
+      replacer.replace(dom, Lang.new('ja'))
+
+      inner_html = dom.xpath('//p')[0].inner_html
+      assert_equal('こんにちは<a>World</a>', inner_html)
+    end
+
+    def test_replace_with_complex_value_unballanced_right_alignment
+      replacer = HTMLTextReplacer.new({}, {
+        'Hello <a>World</a>' => {'ja' => [{'data' => 'こんにちは<a>World</a>!'}]}
+      })
+
+      dom = Wovnrb.get_dom('<p>Hello <a>World</a></p>')
+      replacer.replace(dom, Lang.new('ja'))
+
+      inner_html = dom.xpath('//p')[0].inner_html
+      assert_equal('こんにちは<a>World</a>!', inner_html)
+    end
+  end
+end
