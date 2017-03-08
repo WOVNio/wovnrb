1b9095f6657fc079381fcda1e31c4f79f95d1fe4
toinoudivad <antoine@wovn.io>
2017-03-08 15:22:16 +0900

    ignore js links, tests

--- lib/wovnrb/html_replacers/link_replacer.rb
+++ lib/wovnrb/html_replacers/link_replacer.rb
@@ -11,6 +11,7 @@ module Wovnrb
 
         href = node.get_attribute('href')
         next if href =~ /^\s*\{\{.+\}\}\s*$/
+        next if href =~ /javascript:void\(/
         new_href = lang.add_lang_code(href, @pattern, @headers)
         node.set_attribute('href', new_href)
       end
