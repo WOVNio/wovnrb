d35bf142354828fc48bed05bef5bf48778f45226
kyuden <msmsms.um@gmail.com>
2017-05-24 18:23:10 +0900

    Handle input placeholder

--- lib/wovnrb/html_replacers/input_replacer.rb
+++ lib/wovnrb/html_replacers/input_replacer.rb
@@ -5,23 +5,33 @@ module Wovnrb
     end
 
     def replace(dom, lang)
-      dom.xpath('//input').select { |node|
+      dom.xpath('//input').each do |node|
         next if wovn_ignore?(node)
-        (node.get_attribute('value') &&
-        node.get_attribute('value') != '' &&
-        node.get_attribute('type') &&
-        node.get_attribute('type') != 'text' &&
-        node.get_attribute('type') != 'hidden' &&
-        node.get_attribute('type') != 'password' &&
-        node.get_attribute('type') != 'url' &&
-        node.get_attribute('type') != 'search')
-      }.each do |node|
-        node_value = node.get_attribute('value').strip
-        # shouldn't need size check, but for now...
-        if @text_index[node_value] && @text_index[node_value][lang.lang_code] && @text_index[node_value][lang.lang_code].size > 0
-          node.set_attribute('value', replace_text(node.get_attribute('value'), @text_index[node_value][lang.lang_code][0]['data']))
-        end
+
+        set_attribute('value', node, lang)       if replaceable_value? node
+        set_attribute('placeholder', node, lang) if replaceable_placeholder? node
       end
+    end
+
+    private
+
+    def set_attribute(name, node, lang)
+      node_value = node.get_attribute(name).strip
+      if @text_index[node_value] && @text_index[node_value][lang.lang_code] && @text_index[node_value][lang.lang_code].size > 0
+        node.set_attribute(name, replace_text(node_value, @text_index[node_value][lang.lang_code][0]['data']))
+      end
+    end
+
+    def replaceable_value?(node)
+      return false unless node.get_attribute('type') == 'submit'
+
+      attribute_value = node.get_attribute('value')
+      attribute_value && !attribute_value.empty?
+    end
+
+    def replaceable_placeholder?(node)
+      attribute_placeholder = node.get_attribute('placeholder')
+      attribute_placeholder && !attribute_placeholder.empty?
     end
   end
 end
