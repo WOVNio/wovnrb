02bc6723828ce242423505259c794226a45714b6
Adrien Bougouin <adrien@wovn.io>
2018-10-26 08:31:59 +0900

    complete ApiTranslator tests

--- test/lib/api_translator_test.rb
+++ test/lib/api_translator_test.rb
@@ -2,33 +2,29 @@ require 'test_helper'
 
 module Wovnrb
   class ApiTranslatorTest < WovnMiniTest
-    # TODO: test extra custom_lang_aliases inserted too
-
     def test_translate
-      assert_translation('test.html', 'test_translated.html', true, true)
+      assert_translation('test.html', 'test_translated.html', true)
     end
 
     def test_translate_falls_back_to_original_body_if_exception
       Net::HTTP.any_instance.expects(:request).raises
-      assert_translation('test.html', 'test_translated.html', false, false)
+      assert_translation('test.html', 'test_translated.html', false, nil)
     end
 
-    # def test_translate_falls_back_to_original_body_if_api_error
-      # # TODO: create fake api error
-      # assert_translation('test.html', 'test_translated.html', true, false)
-    # end
+    def test_translate_falls_back_to_original_body_if_api_error
+      assert_translation('test.html', 'test_translated.html', false, { status_code: 500 })
+    end
 
-    # def test_translate_falls_back_to_original_body_if_api_response_is_not_compressed
-      # # TODO: create non compressed response
-      # assert_translation('test.html', 'test_translated.html', true, false)
-    # end
+    def test_translate_falls_back_to_original_body_if_api_response_is_not_compressed
+      assert_translation('test.html', 'test_translated.html', false, { encoding: 'unknown', status_code: 200 })
+    end
 
     private
 
-    def assert_translation(original_html_fixture, translated_html_fixture, fake_request, success_expected)
+    def assert_translation(original_html_fixture, translated_html_fixture, success_expected, response = { encoding: 'gzip', status_code: 200 })
       original_html = File.read("test/fixtures/html/#{original_html_fixture}")
       translated_html = File.read("test/fixtures/html/#{translated_html_fixture}")
-      actual_translated_html = translate(original_html, translated_html, fake_request)
+      actual_translated_html = translate(original_html, translated_html, response)
 
       if success_expected
         assert_equal(actual_translated_html, translated_html)
@@ -37,35 +33,48 @@ module Wovnrb
       end
     end
 
-    def translate(original_html, translated_html, fake_request)
+    def translate(original_html, translated_html, response)
+      settings = {
+        'project_token' => '123456',
+        'custom_lang_aliases' => { 'ja' => 'Japanese' },
+        'default_lang' => 'en',
+        'url_pattern' => 'subdomain',
+        'url_pattern_reg' => '^(?<lang>[^.]+).'
+      }
       store = Wovnrb::Store.instance
-      store.update_settings({ 'project_token' => '123456'})
+      store.update_settings(settings)
       headers = Wovnrb::Headers.new(
         Wovnrb.get_env('url' => "http://fr.wovn.io/test"),
-        Wovnrb.get_settings(
-          'default_lang' => 'en',
-          'url_pattern' => 'subdomain',
-          'url_pattern_reg' => '^(?<lang>[^.]+).'
-        )
+        Wovnrb.get_settings(settings)
       )
       api_translator = ApiTranslator.new(store, headers)
-      translation_request_stub = stub_translation_api_request(store, headers, original_html, translated_html) if fake_request
+      translation_request_stub = stub_translation_api_request(store, headers, original_html, translated_html, response)
 
       actual_translated_html = api_translator.translate(original_html)
-      assert_requested(translation_request_stub, times: 1) if fake_request
+      assert_requested(translation_request_stub, times: 1) if translation_request_stub
       actual_translated_html
     end
 
-    # TODO: encoding and status as arguments
-    def stub_translation_api_request(store, headers, original_html, translated_html)
-      cache_key = generate_cache_key(store, original_html)
-      api_url = "wovn.global.ssl.fastly.net/v0/translation?cache_key=#{cache_key}"
-      compressed_response = compress("{\"body\": \"#{translated_html.gsub("\n", '\n')}\"}")
-      response_headers = { 'Content-Encoding' => 'gzip' }
-      # TODO: stub based on data sent too
-      stub = stub_request(:post, api_url).to_return(status: 200, body: compressed_response, headers: response_headers)
+    def stub_translation_api_request(store, headers, original_html, translated_html, response)
+      if response
+        cache_key = generate_cache_key(store, original_html)
+        api_url = "wovn.global.ssl.fastly.net/v0/translation?cache_key=#{cache_key}"
+        compressed_data = compress(generate_data(original_html))
+        headers = {
+          'Accept' => '*/*',
+          'Accept-Encoding' => 'gzip',
+          'Content-Length' => compressed_data.bytesize,
+          'Content-Type' => 'application/octet-stream',
+          'User-Agent' => 'Ruby'
+        }
+        compressed_response = compress("{\"body\":\"#{translated_html.gsub("\n", '\n')}\"}")
+        response_headers = { 'Content-Encoding' => response[:encoding] }
+        stub = stub_request(:post, api_url)
+          .with(body: compressed_data, headers: headers)
+          .to_return(status: response[:status_code], body: compressed_response, headers: response_headers)
 
-      stub
+        stub
+      end
     end
 
     def generate_cache_key(store, original_html)
@@ -74,6 +83,21 @@ module Wovnrb
       escaped_key = CGI.escape("token=123456&settings_hash=#{settings_hash}&body_hash=#{body_hash}&path=/test&lang=fr")
 
       "(#{escaped_key})"
+    end
+
+    def generate_data(original_html)
+      data = {
+        'url' => 'http://wovn.io/test',
+        'token' => '123456',
+        'lang_code' => 'fr',
+        'url_pattern' => 'subdomain',
+        'product' => 'WOVN.rb',
+        'version' => VERSION,
+        'body' => original_html,
+        'custom_lang_aliases' => '{"ja":"Japanese"}'
+      }
+
+      data.map { |key, value| "#{key}=#{CGI.escape(value)}" }.join('&')
     end
 
     def compress(string)
