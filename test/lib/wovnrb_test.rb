d5f96b742ad9494c5a3de674262f4a82aa660835
Adrien Bougouin <adrien@wovn.io>
2018-11-12 18:36:47 +0900

    set default value for 'wovn_dev_mode' setting (#133)
    * set default value for 'wovn_dev_mode' setting

--- test/lib/wovnrb_test.rb
+++ test/lib/wovnrb_test.rb
@@ -38,7 +38,7 @@ class WovnrbTest < Minitest::Test
     bodies = ['<html><body><h1>Mr. Belvedere Fan Club</h1>',
               '<div><p>Hello</p></div>',
               '</body></html>'].join
-    expected_bodies = ["<html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"><script src=\"//j.dev-wovn.io:3000/1\" async=\"true\" data-wovnio=\"key=123456&amp;amp;backend=true&amp;amp;currentLang=ja&amp;amp;defaultLang=en&amp;amp;urlPattern=subdomain&amp;amp;langCodeAliases={}&amp;amp;version=WOVN.rb_#{Wovnrb::VERSION}\" data-wovnio-type=\"fallback_snippet\"></script><link rel=\"alternate\" hreflang=\"en\" href=\"http://page.com/\"></head><body><h1>Mr. Belvedere Fan Club</h1><div><p>Hello</p></div></body></html>"].join
+    expected_bodies = ["<html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"><script src=\"//j.wovn.io/1\" async=\"true\" data-wovnio=\"key=123456&amp;amp;backend=true&amp;amp;currentLang=ja&amp;amp;defaultLang=en&amp;amp;urlPattern=subdomain&amp;amp;langCodeAliases={}&amp;amp;version=WOVN.rb_#{Wovnrb::VERSION}\" data-wovnio-type=\"fallback_snippet\"></script><link rel=\"alternate\" hreflang=\"en\" href=\"http://page.com/\"></head><body><h1>Mr. Belvedere Fan Club</h1><div><p>Hello</p></div></body></html>"].join
 
     assert_switch_lang('en', 'ja', bodies, expected_bodies, true)
   end
