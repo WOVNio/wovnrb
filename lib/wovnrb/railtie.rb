28491ef3c2b69de18da624e3db40ac1c56bb6c54
Adrien Bougouin <adrien@wovn.io>
2019-06-18 13:45:02 +0900

    Allow Rails Middleware Management by User
    WOVN.rb railtie changes so that `Wovnrb::Interceptor` middleware is added
    only if user did not add it themselves. This change allows for
    configuring middlewares that places before `Wovnrb::Interceptor` to decide
    whether or not `wovn_disable` should be set to true.
    `previous_middleware` setting is no longer needed.
    
    Following is a Rails setup example.
      `config/application.rb`
        ```
    
        class TestMiddleware
          def initialize(app)
            @app = app
          end
    
          def call(env)
            Rack::Request.new(env).update_param('wovn_disable', true)
            return @app.call(env)
          end
        end
    
        module MyApp
          class Application < Rails::Application
            # ...
            config.middleware.insert_before(0, TestMiddleware)
            config.middleware.insert_after(TestMiddleware, Wovnrb::Interceptor)
            # ...
          end
        end
        ```

--- lib/wovnrb/railtie.rb
+++ lib/wovnrb/railtie.rb
@@ -1,20 +1,17 @@
 module Wovnrb
+  def self.middleware_inserted?(app)
+    app.middleware.send(:operations).each do |_, middlewares, _|
+      return true if middlewares.include?(Wovnrb::Interceptor)
+    end
+
+    false
+  end
+
   class Railtie < Rails::Railtie
     initializer 'wovnrb.configure_rails_initialization' do |app|
-      previous_middleware = app.config.wovnrb.symbolize_keys[:previous_middleware] || 0
-
-      app.middleware.insert_before(previous_middleware, Wovnrb::Interceptor)
-      # begin
-      #  app.middleware.insert_before(Rack::Runtime, Wovnrb::Interceptor)
-      # rescue
-      #  app.middleware.insert_before(0, Wovnrb::Interceptor)
-      # end
-
-      # if Rails.env.development? && config.respond_to?(:wovnrb)
-      #  config.after_initialize do
-      #    config.wovnrb[:project_token] = User.first.short_token
-      #  end
-      # end
+      unless Wovnrb.middleware_inserted?(app)
+        app.middleware.insert_before(0, Wovnrb::Interceptor)
+      end
     end
   end
 end
