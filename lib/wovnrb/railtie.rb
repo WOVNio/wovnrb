fb22be44cd01bf6cd6450e9ef86f1834c1b5482d
Adrien Bougouin <adrien@wovn.io>
2019-07-11 10:49:12 +0900

    Add Rack::Deflater Support in railtie.rb (#150)
    * Add Rack::Deflate Support in railtie.rb
    
    Rack::Deflate compresses response rendering WOVN.rb inable to translate
    content. This change ensures that WOVN.rb middleware installs itself
    after Racke::Deflete middleware so that it can process data before
    Rack::Deflate compresses it.

--- lib/wovnrb/railtie.rb
+++ lib/wovnrb/railtie.rb
@@ -1,7 +1,7 @@
 module Wovnrb
-  def self.middleware_inserted?(app)
+  def self.middleware_inserted?(app, middleware)
     app.middleware.send(:operations).each do |_, middlewares, _|
-      return true if middlewares.include?(Wovnrb::Interceptor)
+      return true if middlewares.include?(middleware)
     end
 
     false
@@ -9,7 +9,13 @@ module Wovnrb
 
   class Railtie < Rails::Railtie
     initializer 'wovnrb.configure_rails_initialization' do |app|
-      app.middleware.insert_before(0, Wovnrb::Interceptor) unless Wovnrb.middleware_inserted?(app)
+      unless Wovnrb.middleware_inserted?(app, Wovnrb::Interceptor)
+        if defined?(Rack::Deflater) && Wovnrb.middleware_inserted?(app, Rack::Deflater)
+          app.middleware.insert_after(Rack::Deflater, Wovnrb::Interceptor)
+        else
+          app.middleware.insert_before(0, Wovnrb::Interceptor)
+        end
+      end
     end
   end
 end
