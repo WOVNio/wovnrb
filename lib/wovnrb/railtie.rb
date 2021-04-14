b08da578aafbc3f0de219b8112c731c5907cad0d
Brett Stewart <brett@wovn.io>
2021-04-14 15:31:01 +0900

    (3.5.0) Add setting for disabling automatically inserting middleware. Change installation flow. (#170)

--- lib/wovnrb/railtie.rb
+++ lib/wovnrb/railtie.rb
@@ -1,21 +1,7 @@
 module Wovnrb
-  def self.middleware_inserted?(app, middleware)
-    app.middleware.send(:operations).each do |_, middlewares, _|
-      return true if middlewares&.include?(middleware)
-    end
-
-    false
-  end
-
   class Railtie < Rails::Railtie
     initializer 'wovnrb.configure_rails_initialization' do |app|
-      unless Wovnrb.middleware_inserted?(app, Wovnrb::Interceptor)
-        if defined?(Rack::Deflater) && Wovnrb.middleware_inserted?(app, Rack::Deflater)
-          app.middleware.insert_after(Rack::Deflater, Wovnrb::Interceptor)
-        else
-          app.middleware.insert_before(0, Wovnrb::Interceptor)
-        end
-      end
+      app.middleware.insert_before(0, Wovnrb::Interceptor) if Rails.configuration.wovnrb[:install_middleware]
     end
   end
 end
--- lib/wovnrb/version.rb
+++ lib/wovnrb/version.rb
@@ -1,3 +1,3 @@
 module Wovnrb
-  VERSION = '2.6.0'.freeze
+  VERSION = '3.0.0'.freeze
 end
