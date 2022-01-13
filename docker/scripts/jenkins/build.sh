1d909c1a91defd0d75edeece4aaa9a0bd91370ac
maduranga <maduranga.liyanage@gmail.com>
2022-01-13 10:51:35 +0900

    add service update

--- docker/scripts/jenkins/build.sh
+++ docker/scripts/jenkins/build.sh
@@ -3,9 +3,11 @@ set -eux
 export AWS_PROFILE="wovn-code-staging-pipeline"
 export AWS_REGION="us-west-2"
 export ECR_HOST="257024234524.dkr.ecr.us-west-2.amazonaws.com"
-export REPO_NAME_WOVNRB="wovnrb"
-export REPO_NAME_NGINX="wovnrb-nginx"
-export CLUSTER_NAME="wovn-library-testing"
+REPO_NAME_WOVNRB="wovnrb"
+REPO_NAME_NGINX="wovnrb-nginx"
+CLUSTER_NAME="wovn-library-testing"
+TASKDEF_FAMILY_NAME="wovnrb"
+ECS_SERVICE_NAME="wovnrb"
 PROJECT_DIR=$(dirname "$0")/../../..
 
 commit_hash=$(git rev-parse --short HEAD)
@@ -32,4 +34,12 @@ TASKDEF_REVISION=$(aws ecs register-task-definition \
                          --cli-input-json file://$(pwd)/taskdef.json \
                       | jq ."taskDefinition.revision")
 echo "${TASKDEF_REVISION}"
+
+echo "Start ECS Rolling deploy. Update ${ECS_SERVICE_NAME} by ${TASKDEF_FAMILY_NAME}:${TASKDEF_REVISION}"
+    aws ecs update-service \
+      --profile "${AWS_PROFILE}" --region "${AWS_REGION}" \
+      --cluster "${CLUSTER_NAME}" \
+      --service "${ECS_SERVICE_NAME}" \
+      --task-definition "${TASKDEF_FAMILY_NAME}:${TASKDEF_REVISION}"
+
 cd -
