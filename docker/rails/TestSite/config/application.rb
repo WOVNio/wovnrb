e26e606ffc309d7dc1e98fd884ad88f767d1454c
maduranga <maduranga.liyanage@gmail.com>
2022-02-08 20:17:25 +0900

    cleanup

--- docker/rails/TestSite/config/application.rb
+++ docker/rails/TestSite/config/application.rb
@@ -1,27 +1,16 @@
-require_relative 'boot'
-
-require 'rails/all'
-
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module TestSite
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.hosts.clear
 
     config.wovnrb = {
-      :project_token => '1S6qyK',
+      :project_token => 'EnS!t3',
       :default_lang => 'en',
-      :supported_langs => ['en','ja'],
+      :supported_langs => ['en', 'ja', 'fr'],
       :url_pattern => 'path',
       :install_middleware => true
     }
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
   end
 end
