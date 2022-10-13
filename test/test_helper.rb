d852d9804194f6391b671551495249d203af188b
Ta Dat <ttdat1@gmail.com>
2022-10-13 11:33:49 +0900

    rubocop

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,7 @@ require 'pry'
 
 # save to CircleCI's artifacts directory if we're on CircleCI
 if ENV['CIRCLE_ARTIFACTS']
-  dir = File.join(ENV['CIRCLE_ARTIFACTS'], 'coverage')
+  dir = File.join(ENV.fetch('CIRCLE_ARTIFACTS', nil), 'coverage')
   SimpleCov.coverage_dir(dir)
 end
 
