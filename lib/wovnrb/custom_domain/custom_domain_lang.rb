e6a94c78ac40432527f0c74ace9b19595d5cd0b3
Bryan Zerrudo <bryan.zerrudo@wovn.io>
2023-03-09 16:32:25 +0900

    refactor: Port methods from html-swapper

--- lib/wovnrb/custom_domain/custom_domain_lang.rb
+++ lib/wovnrb/custom_domain/custom_domain_lang.rb
@@ -5,33 +5,27 @@ module Wovnrb
 
     def initialize(host, path, lang)
       @host = host
-      @path = path[-1] == '/' ? path : "#{path}/"
+      @path = path.end_with?('/') ? path : "#{path}/"
       @lang = lang
     end
 
     # @param uri [Addressable::URI]
-    def match?(uri)
-      host = uri.host
-      path = uri.path.presence || '/'
-      @host.casecmp(host).zero? && path_is_equal_or_subset_of(@path, path)
+    def match?(parsed_uri)
+      @host.casecmp?(parsed_uri.host) && path_is_equal_or_subset_of?(@path, parsed_uri.path)
     end
 
     def host_and_path_without_trailing_slash
       host_and_path = @host + @path
-      host_and_path[-1] == '/' ? host_and_path[0..-2] : host_and_path
+      host_and_path.end_with?('/') ? host_and_path.delete_suffix('/') : host_and_path
     end
 
     private
 
-    def path_is_equal_or_subset_of(orig_path, test_path)
-      # split by delimiter and remove spaces and empty strings
-      orig_segments = orig_path.split('/').map(&:strip).select(&:present?)
-      test_segments = test_path.split('/').map(&:strip).select(&:present?)
+    def path_is_equal_or_subset_of?(path1, path2)
+      path1_segments = path1.split('/').reject(&:empty?)
+      path2_segments = path2.split('/').reject(&:empty?)
 
-      length = orig_segments.length
-      diff = orig_segments.slice(0, length) <=> test_segments.slice(0, length)
-
-      diff == 0
+      path1_segments == path2_segments.slice(0, path1_segments.length)
     end
   end
 end
--- lib/wovnrb/custom_domain/custom_domain_langs.rb
+++ lib/wovnrb/custom_domain/custom_domain_langs.rb
@@ -3,39 +3,29 @@ require 'wovnrb/custom_domain/custom_domain_lang'
 module Wovnrb
   # Represents a list of custom domains with corresponding languages
   class CustomDomainLangs
-    attr_accessor :custom_domain_langs
-
-    def initialize(custom_domain_langs_settings_array)
-      @custom_domain_langs = {}
-      custom_domain_langs_settings_array.each do |lang_code, config|
-        url_with_protocol = add_protocol_if_needed(config['url'])
-        parsed_url = Addressable::URI.parse(url_with_protocol)
-
-        @custom_domain_langs[lang_code] = CustomDomainLang.new(parsed_url.host, parsed_url.path, lang_code)
+    def initialize(setting)
+      @custom_domain_langs = setting.map do |lang_code, config|
+        parsed_uri = Addressable::URI.parse(add_protocol_if_needed(config['url']))
+        CustomDomainLang.new(parsed_uri.host, parsed_uri.path, lang_code)
       end
     end
 
     def custom_domain_lang_by_lang(lang_code)
-      @custom_domain_langs[lang_code]
+      @custom_domain_langs.find { |c| c.lang == lang_code }
     end
 
-    def custom_domain_lang_by_url(url)
-      # "/" path will naturally match every URL, so by comparing longest paths first we will get the best match
-      sorted_custom_domain_langs = custom_domain_langs.values.sort do |a, b|
-        a.path.length <= b.path.length ? 1 : -1
-      end
-      url_with_protocol = add_protocol_if_needed(url)
-      parsed_url = Addressable::URI.parse(url_with_protocol)
-      parsed_url.path = '/' if parsed_url.path.blank?
+    def custom_domain_lang_by_url(uri)
+      parsed_uri = Addressable::URI.parse(add_protocol_if_needed(uri))
 
-      sorted_custom_domain_langs.find do |custom_domain_lang|
-        custom_domain_lang.match?(parsed_url)
-      end
+      # "/" path will naturally match every URL, so by comparing longest paths first we will get the best match
+      @custom_domain_langs
+        .sort_by { |c| -c.path.length }
+        .find { |c| c.match?(parsed_uri) }
     end
 
     def to_html_swapper_hash
       result = {}
-      custom_domain_langs.each do |_lang_code, custom_domain_lang|
+      @custom_domain_langs.each do |custom_domain_lang|
         result[custom_domain_lang.host_and_path_without_trailing_slash] = custom_domain_lang.lang
       end
       result
